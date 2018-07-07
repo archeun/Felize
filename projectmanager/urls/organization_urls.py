2f34b26d0a1b4912ec3ac63c85ba648398b69279
ArunaTebel <arunatebel@hotmail.com>
2018-07-07 22:58:18 +0530

    removing unnecessary comments

--- projectmanager/urls/organization_urls.py
+++ projectmanager/urls/organization_urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from projectmanager import views
 
 urlpatterns = [
-    # ex: /pm/organizations
     path('organizations', views.organization_list, name='organization_list'),
-    # ex: /pm/organizations/5/
     path('organizations/<int:organization_id>/', views.organization_details, name='organization_details'),
 ]
--- projectmanager/urls/project_urls.py
+++ projectmanager/urls/project_urls.py
@@ -3,9 +3,6 @@ from django.urls import path
 from projectmanager import views
 
 urlpatterns = [
-    # ex: /pm/projects
-
     path('projects', views.ProjectListView.as_view(), name='project_list'),
-    # ex: /pm/projects/5/
     path('projects/<int:pk>/', views.ProjectDetailView.as_view(), name='project_details'),
 ]
